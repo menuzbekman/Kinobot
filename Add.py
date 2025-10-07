c45dc543010c2889f2977f5ed50d0df3b32a8707
menuzbekman <diyormen47@gmail.com>
2025-10-07 20:25:08 +0500

    Create Add.py

---
+++ Add.py
@@ -0,0 +1,27 @@
+import telebot
+from flask import Flask, request
+import os
+
+TOKEN = "BU_YERGA_SENING_BOT_TOKENINGNI_YOZ"
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Salom! Bu bot Render’da 24/7 ishlayapti 🚀")
+
+@server.route(f'/{TOKEN}', methods=['POST'])
+def getMessage():
+    json_str = request.stream.read().decode('UTF-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "!", 200
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=f"https://kinobot-he20.onrender.com/{TOKEN}")
+    return "Bot ishga tushdi!", 200
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
